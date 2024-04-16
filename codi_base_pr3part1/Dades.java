1b36bda25a8ceee9d36dafd4cd9d130e0cfa2616
albert <you@example.com>
2024-04-16 11:24:50 +0200

    Codigo base y enunciado

---
+++ codi_base_pr3part1/Dades.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prog2.model;
+
+/**
+ *
+ * @author dortiz
+ */
+public class Dades {
+    public final static long  VAR_UNIF_SEED = 123;
+    public final static float GUANYS_INICIALS = 0;
+    public final static float PREU_UNITAT_POTENCIA = 1;
+    public final static float PENALITZACIO_EXCES_POTENCIA = 200;
+
+    // Afegir atributs:
+
+    public Dades(){
+        // Inicialitza Atributs
+        this.variableUniforme = new VariableUniforme(VAR_UNIF_SEED);
+        this.insercioBarres = 100;
+        this.reactor = new Reactor();
+        this.sistemaRefrigeracio = new SistemaRefrigeracio();
+        this.generadorVapor = new GeneradorVapor();
+        this.generadorVapor.activa();
+        this.turbina = new Turbina();
+        this.turbina.activa();
+        this.bitacola = new Bitacola();
+        this.dia = 1;
+        this.guanysAcumulats = GUANYS_INICIALS;
+        
+        // Afegeix bombes refrigerants
+        BombaRefrigerant b0 = new BombaRefrigerant(variableUniforme, 0);
+        BombaRefrigerant b1 = new BombaRefrigerant(variableUniforme, 1);
+        BombaRefrigerant b2 = new BombaRefrigerant(variableUniforme, 2);
+        BombaRefrigerant b3 = new BombaRefrigerant(variableUniforme, 3);
+        
+        this.sistemaRefrigeracio.afegirBomba(b0);
+        this.sistemaRefrigeracio.afegirBomba(b1);
+        this.sistemaRefrigeracio.afegirBomba(b2);
+        this.sistemaRefrigeracio.afegirBomba(b3);
+    }
+    
+    /**
+     * Actualitza l'economia de la central. Genera una pàgina econòmica a 
+     * partir de la demanda de potencia actual. Aquesta pàgina econòmica inclou 
+     * beneficis, penalització per excès de potència, costos operatius y 
+     * guanys acumulats.
+     * @param demandaPotencia Demanda de potència actual.
+     */
+    private PaginaEconomica actualitzaEconomia(float demandaPotencia){
+        // Completar 
+    }
+    
+    /**
+     * Actualitza l'estat de la central. El mètodo ha de establir la nova
+     * temperatura del reactor i revisar els components de la central. Si
+     * es troben incidències, s'han de registrar en la pàgina d'incidències
+     * que es proporciona com a paràmetre d'entrada.
+     * @param paginaIncidencies Pàgina d'incidències.
+     */
+    private void actualitzaEstatCentral(PaginaIncidencies paginaIncidencies) {
+        // Completar
+    }
+    
+    public Bitacola finalitzaDia(float demandaPotencia) {
+        // Actualitza economia
+        PaginaEconomica paginaEconomica = actualitzaEconomia(demandaPotencia);
+        
+        // Actualitza estat central i registra incidencies
+        PaginaIncidencies paginaIncidencies = new PaginaIncidencies(dia);
+        actualitzaEstatCentral(paginaIncidencies);
+        
+        // Genera pàgina d'estat
+        PaginaEstat paginaEstat = mostraEstat(demandaPotencia);
+        
+        // Incrementa dia
+        dia += 1;
+        
+        // Guarda pàgines de bitacola
+        bitacola.afegeixPagina(paginaEconomica);
+        bitacola.afegeixPagina(paginaEstat);
+        bitacola.afegeixPagina(paginaIncidencies);
+        
+        // Retorna pàgines
+        Bitacola bitacolaDia = new Bitacola();
+        bitacolaDia.afegeixPagina(paginaEconomica);
+        bitacolaDia.afegeixPagina(paginaEstat);
+        bitacolaDia.afegeixPagina(paginaIncidencies);
+        return bitacolaDia;
+    }
+}
