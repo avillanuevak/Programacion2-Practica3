b1b783f563a9588b712a4e4fe7f5ad55b8eba76a
BERKFAST <brekfast0@gmail.com>
2024-04-30 11:45:06 +0200

    He heho PaginaIncidencias

--- ChenJamesVillanuevaAlbert_Pr31/src/prog2/model/PaginaIncidencies.java
+++ ChenJamesVillanuevaAlbert_Pr31/src/prog2/model/PaginaIncidencies.java
@@ -4,10 +4,54 @@
  */
 package prog2.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Albert
  */
-public class PaginaIncidencies {
+public class PaginaIncidencies extends PaginaBritacola{
+    int nDia;
+    ArrayList<String> llistaIncidencies;
+
+    public int getnDia() {
+        return nDia;
+    }
+
+    public void setnDia(int nDia) {
+        this.nDia = nDia;
+    }
+
+    public ArrayList<String> getLlistaIncidencies() {
+        return llistaIncidencies;
+    }
+
+    public void setLlistaIncidencies(ArrayList<String> llistaIncidencies) {
+        this.llistaIncidencies = llistaIncidencies;
+    }
     
+    public PaginaIncidencies(){
+        llistaIncidencies = new ArrayList<>(); 
+        nDia = 0;
+    }
+    
+    public PaginaIncidencies(int nDia, ArrayList<String> llistaIncidencies){
+        this.nDia = nDia;
+        this.llistaIncidencies = llistaIncidencies;
+    }
+    
+    public void afegeixIncidencia(String descIncidencia){
+        llistaIncidencies.add(descIncidencia);
+    }
+    @Override
+    public String toString(){
+        String mensaje = "";
+        for(String it: llistaIncidencies){
+            mensaje += "Descripció Incidencia: " + it + "\n";
+        }
+        return "# Pàgina Incidencies\n- Dia: " + getnDia() + "\n" + mensaje;
+    }
+    
+    
+
 }
